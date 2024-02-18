c11e923b63f149cc8b939af46c6d76f58543b987
MHG1816 <hammaguibeneess@gmail.com>
2024-02-18 15:35:45 +0100

    replace

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Figtree } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/sidebar";
 import SupabaseProvider from "@/providers/SupabaseProvider";
-import UserProvider from "@/hooks/UserProvider";
+import UserProvider from "@/providers/UserProvider";
 
 const figTree = Figtree({ subsets: ["latin"] });
 
