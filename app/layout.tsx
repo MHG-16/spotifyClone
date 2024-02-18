86f94efc765ceffce832b3189401201a035523d0
MHG1816 <hammaguibeneess@gmail.com>
2024-02-18 13:43:51 +0100

    user provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Figtree } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/sidebar";
 import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/hooks/UserProvider";
 
 const figTree = Figtree({ subsets: ["latin"] });
 
@@ -20,9 +21,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={figTree.className}>
         <SupabaseProvider>
-          <Sidebar>
-            {children}
-          </Sidebar>
+          <UserProvider>
+            <Sidebar>
+              {children}
+            </Sidebar>
+          </UserProvider>
         </SupabaseProvider>
       </body>
     </html>
---
+++ hooks/useUser.tsx
@@ -0,0 +1,85 @@
+import { User } from "@supabase/auth-helpers-nextjs";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useSessionContext, useUser as useSupaUser } from "@supabase/auth-helpers-react";
+
+import { Subscription, UserDetails } from "@/types";
+
+
+type UserContextType = {
+    accessToken: string | null;
+    user: User | null;
+    userDetails: UserDetails | null;
+    isLoading: boolean;
+    subscription: Subscription | null;
+}
+
+export const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export interface Props {
+    [propsName: string]: any;
+}
+
+export const MyUserContextProvider = (props: Props ) => {
+    const { 
+        session,
+        isLoading: isLoadingUser,
+        supabaseClient: supabase
+    } = useSessionContext();
+
+    const user = useSupaUser();
+    const accessToken = session?.access_token ?? null;
+    const [ isLoadingData, setLoadingData ] = useState(false);
+    const [ userDetails, setUserDetails ] = useState<UserDetails | null>(null);
+    const [ subscription, setSubscription ] = useState<Subscription | null>(null);
+    
+    useEffect(() => {
+        const getUserDetails = () => supabase.from("users").select("*").single();
+        const getSubscription = () => supabase.from("subscriptions")
+            .select("*, prices(*, products(*))")
+            .in("status", ["trialing", "active"])
+            .single();
+        if (user && !isLoadingData && !subscription) { 
+            setLoadingData(true);
+
+            Promise.allSettled([getUserDetails(), getSubscription()]).then(
+                (results) => {
+                    const userDetailsPromise = results[0];
+                    const subscriptionPromise = results[1];
+
+                    if (userDetailsPromise.status === "fulfilled") {
+                        setUserDetails(userDetailsPromise.value.data as UserDetails);
+                    }
+
+                    if (subscriptionPromise.status === "fulfilled") {
+                        setSubscription(subscriptionPromise.value.data as Subscription);
+                    }
+
+                    setLoadingData(false);
+                }
+            )
+        } else if (!user && !isLoadingData && !isLoadingUser) {
+            setSubscription(null);
+            setUserDetails(null);
+        }
+    }, [isLoadingData, isLoadingUser, subscription, supabase, user]);
+
+    const value = {
+        accessToken,
+        user,
+        userDetails,
+        isLoading: isLoadingData|| isLoadingUser,
+        subscription
+    }
+
+    return <UserContext.Provider value={value} {...props} />;
+}
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+
+    if (context === undefined) {
+        throw new Error("use user context must be provided within a MyUserContextProvider");
+    }
+
+    return context;
+}
