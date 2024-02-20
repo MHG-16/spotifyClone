1698d195746023912bc30df1374a2fff79ba8ac4
MHG1816 <hammaguibeneess@gmail.com>
2024-02-20 16:40:44 +0100

    get songs

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,11 @@
+import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
-import Image from "next/image";
 
-export default function Home() {
+export const revalidate = 0;
+
+export default async function Home() {
+  const songs = await getSongs();
   return (
     <div className="bg-neutral-900 rounded-lg w-full h-full overflow-hidden overflow-y-auto">
       <Header>
@@ -22,7 +25,7 @@ export default function Home() {
           </h3>
         </div>
         <div>
-          List of songs
+          {songs.map((song) => <div key={song.id}>{song.title}</div>)}
         </div>
       </div>
     </div>
