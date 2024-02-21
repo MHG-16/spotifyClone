873f764846888e1ae105906d09463444f152c6fe
MHG1816 <hammaguibeneess@gmail.com>
2024-02-21 20:46:36 +0100

    Like button

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import MediaItem from "@/components/MediaItem";
+import LikeButton from "@/components/button/LikeButton";
 import { Song } from "@/types";
 
 interface SearchContentProps {
@@ -27,7 +28,7 @@ const SearchContent: React.FC<SearchContentProps> = ({
                         data={song}
                     />
                 </div>
-                {/* TODO: Add Like Button Here */}
+                <LikeButton songId={song.id}/>
             </div>
         ))}
     </div>
