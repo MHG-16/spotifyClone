41c8fa3ae4f226c78465f24d67e5c2fe701accf5
MHG1816 <hammaguibeneess@gmail.com>
2024-02-21 13:55:03 +0100

    get song by title

---
+++ actions/getSongsBytitle.ts
@@ -0,0 +1,29 @@
+import { Song } from "@/types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import getSongs from "./getSongs";
+
+const getSongByTitle = async (title: string): Promise<Song[]> => {
+    const supabase = createServerComponentClient({
+        cookies: cookies
+    });
+
+    if(!title) {
+        // return all the songs when no title is provided.
+        return getSongs();
+    }
+
+    const { data, error } = await supabase
+        .from("song")
+        .select("*")
+        .ilike("title", `%${title}%`)
+        .order("created_at", { ascending: false })
+    
+    if (error) {
+        console.log(error.message);
+    }
+
+    return (data as Song[]) || [];
+};
+
+export default getSongByTitle;
