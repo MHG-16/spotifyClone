9291e73966242165ca207b5747f29cc3f1b7678c
MHG1816 <hammaguibeneess@gmail.com>
2024-02-22 08:08:09 +0100

    onPlay hook

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,15 +1,23 @@
 "use client";
 
 import useGetSongById from "@/hooks/useGetSongById";
+import useLoadSong from "@/hooks/useLoadSongUrl";
 import usePlayer from "@/hooks/usePlayer";
 
 const Player = () => {
   const player = usePlayer();
   const { song } = useGetSongById(player.activeId);
+  
+  const songUrl = useLoadSong(song!);
 
-  // const songUrl = 
+  if (!song || !songUrl || !player.activeId) {
+    return null;
+  }
+
   return (
-    <div>Player</div>
+    <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
+        
+    </div>
   )
 }
 
