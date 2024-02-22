ee9be163fbb738702f6b53bf3c3275a03f8fcb42
MHG1816 <hammaguibeneess@gmail.com>
2024-02-22 21:57:01 +0100

    error page

---
+++ app/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Box from "@/components/Box";
+
+const Error = () => {
+    return (
+        <Box className="h-full flex items-center justify-center">
+            <div className="text-neutal-400">
+                Something went wrong.
+            </div>
+        </Box>
+    )
+}
