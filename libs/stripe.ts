4b400e6bdbdeff6aeabec5d88a78d59257587b99
MHG1816 <hammaguibeneess@gmail.com>
2024-02-23 11:15:26 +0100

    setting

--- libs/stripe.ts
+++ libs/stripe.ts
@@ -5,7 +5,7 @@ export const stripe = new Stripe(
     {
         apiVersion: "2023-10-16",
         appInfo: {
-            name: "Spotify Clone Video",
+            name: "Spotify Clone",
             version: "0.1.0"
         }
     }
