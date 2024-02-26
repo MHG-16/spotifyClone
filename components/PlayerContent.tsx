2b1b063476e9392a67d430cedcac88ff12ef60f0
MHG1816 <hammaguibeneess@gmail.com>
2024-02-26 13:14:30 +0100

    update

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
+// @ts-ignore
 import useSound from "use-sound";
 
 import { Song } from "@/types";
